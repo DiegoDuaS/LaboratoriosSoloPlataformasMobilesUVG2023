439e3a50b5643dadb77d5292ce4e0acaba549178
DiegoDuaS <110642453+DiegoDuaS@users.noreply.github.com>
2023-07-25 11:55:46 -0600

    Funcion Operacion

--- LAB2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ LAB2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,16 @@ class MainActivity {
             }
             return pal
         }
+        fun Operacion(numA: Int, numB: Int, lambda: String): Int{
+            var resultado = 0
+            if(lambda == "+"){
+                resultado = numA + numB
+            }
+            else if(lambda == "-"){
+                resultado = numA - numB
+            }
+            return resultado
+        }
 
 }
 
@@ -23,10 +33,29 @@ fun main(){
     val mainActivity = MainActivity()
     val Listaa: List<Int> = listOf(2,5,7,10,12,4,1)
     val promedio: Double = mainActivity.Promedio(Listaa)
+    println("\nEJERCICIO 1\n Lista: $Listaa \n Promedio: $promedio")
+
     val pares: List<Int> = mainActivity.pares(Listaa)
+    println("\nEJERCICIO 2\n Lista: $Listaa \n Pares: $pares")
+
+
     val palabra = "araña"
-    val palabra2 = "rad"
+    val palabra2 = "radar"
     val pali1: Boolean = mainActivity.Palindrome(palabra)
     val pali2: Boolean = mainActivity.Palindrome(palabra2)
+    println("\nEJERCICIO 3\n Palabra: $palabra \n Palindrome: $pali1 \n Palabra: $palabra2 \n Palindrome: $pali2")
+
+
+    println("\nEJERCICIO 4\n ")
+    val nombres = listOf("Marce", "Jeffrey", "Isa", "Julio")
+    val saludos = nombres.map { "¡Hola, $it!" }
+    saludos.forEach { println(it) }
+
+    println("\nEJERCICIO 4\n ")
+    val num1 = 20
+    val num2 = 5
+    val resultado: Int = mainActivity.Operacion(num1,num2,"+")
+    println("El resultado de la operacion es: $resultado")
+
 
 }
