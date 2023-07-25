e5abd1710a8942fc49bd379cdf6254c136ffa570
DiegoDuaS <110642453+DiegoDuaS@users.noreply.github.com>
2023-07-25 11:03:08 -0600

    FunciónPromedio

--- LAB2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ LAB2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,3 +1,14 @@
 class MainActivity {
-
+        fun Promedio(lista: List<Int>): Double{
+            val suma = lista.reduce { acumulado, elemento -> acumulado + elemento }
+            val promedio = suma.toDouble() / lista.size
+            return promedio
+        }
     }
+
+fun main(){
+    val Listaa: List<Int> = listOf(2,5,7,10,12,4,1)
+    val mainActivity = MainActivity()
+    val promedio: Double = mainActivity.Promedio(Listaa)
+    println("$promedio")
+}
