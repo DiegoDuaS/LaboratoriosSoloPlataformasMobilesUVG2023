4ed4dcc98e4a7b6626a600c2c25dbd4eb49480eb
DiegoDuaS <110642453+DiegoDuaS@users.noreply.github.com>
2023-08-10 07:43:56 -0600

    Compose
    Compose

--- LAB4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ LAB4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,30 +1,20 @@
-package com.example.lab4
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab4.ui.theme.Lab4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lab4Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            // Aquí es donde definirás tu interfaz de usuario con Compose
+            Greeting("Hello, Compose!")
         }
     }
 }
