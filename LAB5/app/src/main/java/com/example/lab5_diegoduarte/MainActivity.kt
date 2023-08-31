f9d9a4c5a2bebc73005878c1995c50b42d710b2b
DiegoDuaS <dua22075@uvg.edu.gt>
2023-08-30 19:47:04 -0600

    Correcciones Parte 1

--- LAB5/app/src/main/java/com/example/lab5_diegoduarte/MainActivity.kt
+++ LAB5/app/src/main/java/com/example/lab5_diegoduarte/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,11 +15,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,11 +42,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab5_diegoduarte.ui.theme.BlueCito
 import com.example.lab5_diegoduarte.ui.theme.BlueOscuro
+import com.example.lab5_diegoduarte.ui.theme.Fondo
+import com.example.lab5_diegoduarte.ui.theme.Fondo1
 import com.example.lab5_diegoduarte.ui.theme.LAB5_DiegoDuarteTheme
+import com.example.lab5_diegoduarte.ui.theme.Tarjetitas
 
 
 data class Venues(val name: String, val place:String)
+data class Conciertos(val name: String, val date: String, val image: Int)
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,137 +63,133 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Pantalla2()
+                    Pantalla1()
+                    //Pantalla2()
                 }
             }
         }
     }
 }
+
 @Composable
 fun Pantalla1(){
+    val ConciertosFav = remember { mutableStateListOf<Conciertos>() }
+    val ConciertosAll = remember { mutableStateListOf<Conciertos>() }
+    ConciertosFav.add(Conciertos("Young Miko", "9 de Septiembre, 2023",R.drawable.youngmiko))
+    ConciertosFav.add(Conciertos("Latin Mafia", "20 de Octube, 2023", R.drawable.latinmafia))
+    ConciertosFav.add(Conciertos("Twice", "2 de Noviembre, 2023", R.drawable.twice))
+    ConciertosFav.add(Conciertos("Feid", "4 de Diciembre, 2023", R.drawable.ferxxo))
+    ConciertosAll.add(Conciertos("Young Miko", "9 de Septiembre, 2023",R.drawable.youngmiko))
+    ConciertosAll.add(Conciertos("Latin Mafia", "20 de Octube, 2023", R.drawable.latinmafia))
+    ConciertosAll.add(Conciertos("Twice", "2 de Noviembre, 2023", R.drawable.twice))
+    ConciertosAll.add(Conciertos("Feid", "4 de Diciembre, 2023", R.drawable.ferxxo))
+
     Column(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .background(color = Fondo),
         horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box(
+            modifier = Modifier
+                .background(color = Fondo1)
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Todo Eventos",
+                style = androidx.compose.material.MaterialTheme.typography.h4,
+                modifier = Modifier
+                    .padding(bottom = 8.dp, top = 8.dp)
+                    .align(Alignment.Center),
+                color = BlueOscuro
+            )
+        }
+        Text(
+            text = "Eventos Favoritos",
+            style = androidx.compose.material.MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(bottom = 8.dp, top = 8.dp),
+            color = BlueOscuro,
+            textAlign = TextAlign.Start,
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ){
+            ConciertosFav.forEach { concierto ->
+                CardConcierto(concierto = concierto)
+            }
+        }
+        Text(
+            text = "Todos Los Conciertos",
+            style = androidx.compose.material.MaterialTheme.typography.h6,
+            modifier = Modifier.padding(bottom = 8.dp, top = 8.dp),
+            color = BlueOscuro
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ){
+            ConciertosAll.forEach { concierto ->
+                CardConcierto(concierto = concierto)
+            }
+        }
+    }
+}
 
+@Composable
+fun CardConcierto(concierto: Conciertos){
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .background(color = Fondo)
+            .width(150.dp)
     ){
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = BlueCito),
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ){
-
-        Text(
-            text = "Todo Eventos",
-            style = androidx.compose.material.MaterialTheme.typography.h4,
-            modifier = Modifier.padding(bottom = 8.dp, top = 8.dp),
-            color = BlueOscuro
-        )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(),
+                .background(color = Tarjetitas),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            EventosConjuntos("Young Miko", "9 de Septiembre, 2023", R.drawable.youngmiko, "Latin Mafia", "4 de Diciembre, 2023",R.drawable.latinmafia)
-            EventosConjuntos(artista1 = "Twice", fecha1 = "4 de Noviembre, 2023", imagen1 = R.drawable.twice, artista2 = "Feid", fecha2 = "20 de Octubre, 2023", imagen2 =  R.drawable.ferxxo)
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .background(color = Tarjetitas)
+            ){
+                Image(
+                    painter = painterResource(id = concierto.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(170.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(
+                text = concierto.name,
+                style = androidx.compose.material.MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth(),
+                color = BlueOscuro,
+                textAlign = TextAlign.Center
+
+            )
+            Text(
+                text = concierto.date,
+                style = androidx.compose.material.MaterialTheme.typography.body2,
+                modifier = Modifier
+                    .padding(vertical = 2.dp)
+                    .fillMaxWidth(),
+                color = BlueOscuro,
+                textAlign = TextAlign.Center
+            )
         }
     }
-
 }
 
-@Composable
-fun EventosConjuntos(artista1: String, fecha1: String, imagen1: Int, artista2: String, fecha2: String, imagen2: Int){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Card(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(8.dp)
-                    .background(Color.White)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = BlueCito),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Image(
-                        painter = painterResource(id = imagen1),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(170.dp)
-                            .clip(RoundedCornerShape(10.dp))
-                    )
-                    Text(
-                        text = artista1,
-                        style = androidx.compose.material.MaterialTheme.typography.h6,
-                        modifier = Modifier
-                            .padding(vertical = 8.dp)
-                            .fillMaxWidth(),
-                        color = BlueOscuro,
-                        textAlign = TextAlign.Center
-
-                    )
-                    Text(
-                        text = fecha1,
-                        style = androidx.compose.material.MaterialTheme.typography.body2,
-                        modifier = Modifier
-                            .padding(vertical = 2.dp)
-                            .fillMaxWidth(),
-                        color = BlueOscuro,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
-
-            Card(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(8.dp)
-                    .background(Color.White)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = BlueCito),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Image(
-                        painter = painterResource(id = imagen2),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(170.dp)
-                            .clip(RoundedCornerShape(10.dp))
-                    )
-                    Text(
-                        text = artista2,
-                        style = androidx.compose.material.MaterialTheme.typography.h6,
-                        modifier = Modifier
-                            .padding(vertical = 8.dp)
-                            .fillMaxWidth(),
-                        color = BlueOscuro,
-                        textAlign = TextAlign.Center
-
-                    )
-                    Text(
-                        text = fecha2,
-                        style = androidx.compose.material.MaterialTheme.typography.body2,
-                        modifier = Modifier
-                            .padding(vertical = 2.dp)
-                            .fillMaxWidth(),
-                        color = BlueOscuro,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
-        }
-}
 @Composable
 fun Pantalla2(){
     val Lista = remember { mutableStateListOf<Venues>() }
@@ -193,7 +201,7 @@ fun Pantalla2(){
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = BlueCito),
+            .background(color = Fondo),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         LazyColumn {
@@ -211,6 +219,7 @@ fun Lugares(lugar:Venues) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
+            .background(color = Fondo)
     ) {
         Row(
             modifier = Modifier
@@ -222,7 +231,7 @@ fun Lugares(lugar:Venues) {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
-                    .background(color = BlueCito)
+                    .background(color = Fondo1)
                     .align(Alignment.CenterVertically)
                     .align(Alignment.CenterVertically)
             ){
@@ -232,6 +241,7 @@ fun Lugares(lugar:Venues) {
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .align(Alignment.Center),
+                    color = Color.Black,
                 )
             }
             Column(
@@ -244,7 +254,7 @@ fun Lugares(lugar:Venues) {
                     style = androidx.compose.material.MaterialTheme.typography.h6,
                     modifier = Modifier
                         .padding(vertical = 2.dp),
-                    color = BlueOscuro,
+
                     textAlign = TextAlign.Start
                 )
                 Text(
@@ -258,6 +268,7 @@ fun Lugares(lugar:Venues) {
             }
             Button(
                 onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Fondo1)
 
             ) {
                 Text("...")
@@ -267,7 +278,7 @@ fun Lugares(lugar:Venues) {
 }
 @Composable
 fun Pantalla3(){
-
+    
 }
 
 @Composable
